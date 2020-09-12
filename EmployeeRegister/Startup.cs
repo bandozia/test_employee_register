90f517e9f43cd9c01ada74decfddddf2ba9d086d
unknown <bruno.andozia@gmail.com>
2020-09-12 11:19:57 -0300

    webapp estrutura

--- EmployeeRegister/Startup.cs
+++ EmployeeRegister/Startup.cs
@@ -20,14 +20,14 @@ namespace EmployeeRegister
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+                
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSpaStaticFiles(conf => conf.RootPath = "webapp");
+            
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+                
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,12 +36,24 @@ namespace EmployeeRegister
             }
 
             app.UseRouting();
-
             app.UseAuthorization();
+            
+            if (!env.IsDevelopment())
+            {
+                app.UseSpaStaticFiles();
+            }
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSpa(spa =>
+            {
+                if (env.IsDevelopment())
+                {
+                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                }
             });
         }
     }
---
+++ readme.md
@@ -0,0 +1,3 @@
+## Projeto teste
+
+![.NET Core](https://github.com/bandozia/test_employee_register/workflows/.NET%20Core/badge.svg)
